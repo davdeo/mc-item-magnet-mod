70aba7e919468f8356e06f0d3cd6f2432b438bdb
David <david.monz@hotmail.com>
2024-03-16 20:53:29 +0100

    init

---
+++ src/main/java/net/davdeo/ItemMagnetMod.java
@@ -0,0 +1,16 @@
+package net.davdeo;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItemMagnetMod implements ModInitializer {
+	public static final String MOD_ID = "itemmagnetmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/davdeo/ItemMagnetModClient.java
@@ -0,0 +1,14 @@
+package net.davdeo;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ItemMagnetModClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
