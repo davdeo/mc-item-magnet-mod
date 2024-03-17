79b7d37e92e938aedb5037cd7ca53c1b4be36ece
David <david.monz@hotmail.com>
2024-03-17 07:59:32 +0100

    Add example mixins

--- src/main/java/net/davdeo/itemmagnetmod/mixin/OtherMixin.java
+++ src/main/java/net/davdeo/itemmagnetmod/mixin/OtherMixin.java
@@ -1,17 +1,16 @@
 package net.davdeo.itemmagnetmod.mixin;
 
 import net.davdeo.itemmagnetmod.ItemMagnetMod;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void loadWorld(CallbackInfo info) {
-		ItemMagnetMod.LOGGER.info("Hello load world");
+@Mixin(TitleScreen.class)
+public class OtherMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		ItemMagnetMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
-
 }
