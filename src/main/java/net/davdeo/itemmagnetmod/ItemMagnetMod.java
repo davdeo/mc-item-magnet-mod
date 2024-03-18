095d02009967b01d69a9f668b21246368a347d91
David <david.monz@hotmail.com>
2024-03-18 22:26:10 +0100

    Update TODOs

--- src/main/java/net/davdeo/itemmagnetmod/ItemMagnetMod.java
+++ src/main/java/net/davdeo/itemmagnetmod/ItemMagnetMod.java
@@ -2,21 +2,30 @@ package net.davdeo.itemmagnetmod;
 
 import net.davdeo.itemmagnetmod.item.ModItemGroups;
 import net.davdeo.itemmagnetmod.item.ModItems;
-import net.davdeo.itemmagnetmod.item.custom.ItemMagnetItem;
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.minecraft.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /*
- * TODO - Manage durability of magnet
+ * BUG - Glint / isActive state is set on Item level instead of ItemStack level -> leads to all magnet having the same state
+ * 	   - could be solved by using nbt data on stack instead of isActive on Item instance
+ * BUG - creating a new magnet has isActive flag set to true
+ * 	   - could be solved by solving the other BUG
+ * BUG - Fix magnet inactive after rejoin of world
+ * 	   - could be solved by solving the other BUG
+ * BUG - Fix "jumping" of affected items after disable of magnet
+ *
  * TODO - Add recipe to craft magnet
  *  	- Add magnet core
  * 		- Edit loot-tables for magnet core to spawn in chests
- * TODO - Fix magnet inactive after rejoin of world
- * TODO - Fix "jumping" of affected items after disable of magnet
+ * TODO - Prevent magnet from breaking -> convert it to broken magnet upon reaching 1 durability
+ * TODO - Add different magnet versions
+ *  	- Hardened magnet -> more durability (maybe enchant with unbreaking?)
+ * 		- Crafter magnet -> auto crafts recipe upon collecting necessary res
+* 		- Container magnet  -> has a container (shulker box) attached and moves items into it
+ * 		- Ranged magnet -> More range
+ * 		- Ender magnet -> Directly inserts items into the inventory
  *
  */
 
