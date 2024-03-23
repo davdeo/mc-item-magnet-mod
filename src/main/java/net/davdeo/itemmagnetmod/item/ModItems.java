895919ea269a5f379ba1c908ec3d01d7ab03cb90
David <david.monz@hotmail.com>
2024-03-23 16:07:54 +0100

    Set maxcount of broken magnet to 1

--- src/main/java/net/davdeo/itemmagnetmod/item/ModItems.java
+++ src/main/java/net/davdeo/itemmagnetmod/item/ModItems.java
@@ -17,7 +17,7 @@ public class ModItems {
             new ItemMagnetItem(new FabricItemSettings().maxDamage(1024)));
 
     public static final Item ITEM_MAGNET_BROKEN = registerItem("item_magnet_broken",
-            new Item(new FabricItemSettings()));
+            new Item(new FabricItemSettings().maxCount(1)));
     public static final Item MAGNET_CORE = registerItem("magnet_core",
             new Item(new FabricItemSettings()));
 
