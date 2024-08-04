86a57e49687f8627bf4a009862809c6a8ea923c8
davdeo <david.monz@hotmail.com>
2024-08-04 14:13:35 +0200

    Fix item durability loss and break sound

--- src/main/java/net/davdeo/itemmagnetmod/event/custom/PickupItemEvent.java
+++ src/main/java/net/davdeo/itemmagnetmod/event/custom/PickupItemEvent.java
@@ -1,11 +1,16 @@
 package net.davdeo.itemmagnetmod.event.custom;
 
+import net.davdeo.itemmagnetmod.ItemMagnetMod;
 import net.davdeo.itemmagnetmod.item.ModItems;
 import net.davdeo.itemmagnetmod.util.ItemMagnetHelper;
 import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 
 public interface PickupItemEvent {
@@ -25,6 +30,7 @@ public interface PickupItemEvent {
     ActionResult onPickup(PlayerEntity player, int count);
 
     static ActionResult onPickupEvent(PlayerEntity player, int pickedUpItemsCount) {
+        ItemMagnetMod.LOGGER.debug("On pickup event");
         int activeMagnetInventoryIndex = ItemMagnetHelper.getFirstActiveMagnetInventoryIndex(player);
 
         if (activeMagnetInventoryIndex == -1) {
@@ -33,20 +39,32 @@ public interface PickupItemEvent {
 
         ItemStack activeMagnet = player.getInventory().getStack(activeMagnetInventoryIndex);
 
-        boolean replaceWithBrokenMagnet = activeMagnet.getMaxDamage() - activeMagnet.getDamage() - pickedUpItemsCount <= 0;
+        ServerPlayerEntity serverPlayer = null;
+        if (player instanceof ServerPlayerEntity serverPlayerEntity) {
+            serverPlayer = serverPlayerEntity;
+        }
 
-        // TODO Fix magnet durability loss
-//        activeMagnet.damage(
-//                pickedUpItemsCount,
-//                player.getWorld(),
-//                player,
-//                item -> {
-//                    LOGGER.debug("hi");
-//                }
-//        );
+        int newDamage = activeMagnet.getDamage() + pickedUpItemsCount;
 
-        if (replaceWithBrokenMagnet) {
+        if (serverPlayer != null && pickedUpItemsCount != 0) {
+            Criteria.ITEM_DURABILITY_CHANGED.trigger(serverPlayer, activeMagnet, newDamage);
+        }
+
+        activeMagnet.setDamage(newDamage);
+
+        if (newDamage >= activeMagnet.getMaxDamage()) {
+            activeMagnet.decrement(1);
+
             player.getInventory().setStack(activeMagnetInventoryIndex, new ItemStack(ModItems.ITEM_MAGNET_BROKEN));
+            if (serverPlayer != null && !serverPlayer.getWorld().isClient) {
+                serverPlayer.getWorld().playSound(
+                        null,
+                        serverPlayer.getBlockPos(),
+                        SoundEvents.ENTITY_ITEM_BREAK,
+                        SoundCategory.PLAYERS,
+                        1f, 1f
+                    );
+            }
         }
 
         return ActionResult.PASS;
