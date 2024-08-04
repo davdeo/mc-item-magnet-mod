6237d8363c631819443677250ecb506e02811054
davdeo <david.monz@hotmail.com>
2024-08-04 19:16:25 +0200

    Add missing constructor

--- src/main/java/net/davdeo/itemmagnetmod/event/ModEvents.java
+++ src/main/java/net/davdeo/itemmagnetmod/event/ModEvents.java
@@ -4,6 +4,10 @@ import net.davdeo.itemmagnetmod.ItemMagnetMod;
 import net.davdeo.itemmagnetmod.event.custom.PickupItemEvent;
 
 public class ModEvents {
+    private ModEvents() {
+        super();
+    }
+
     public static void registerModEvents() {
         ItemMagnetMod.LOGGER.info("Registering mod events for: " + ItemMagnetMod.MOD_ID);
 
