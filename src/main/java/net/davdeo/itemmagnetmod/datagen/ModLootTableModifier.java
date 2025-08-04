05617d81db0aff92ccc07855c82ef4f9bf8ec6c9
davdeo <david.monz@hotmail.com>
2025-08-04 19:33:57 +0200

    Update loottables

--- src/main/java/net/davdeo/itemmagnetmod/datagen/ModLootTableModifier.java
+++ src/main/java/net/davdeo/itemmagnetmod/datagen/ModLootTableModifier.java
@@ -33,15 +33,16 @@ public class ModLootTableModifier {
     }
 
     public static void modifyLootTables() {
-        registerChestLoot(LootTables.BASTION_TREASURE_CHEST, 2, 0.35f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
-        registerChestLoot(LootTables.BASTION_BRIDGE_CHEST, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
-        registerChestLoot(LootTables.BASTION_HOGLIN_STABLE_CHEST, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
-        registerChestLoot(LootTables.BASTION_OTHER_CHEST, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(LootTables.STRONGHOLD_LIBRARY_CHEST, 1, 0.02f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
 
-        registerChestLoot(LootTables.ANCIENT_CITY_CHEST, 1, 0.35f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(LootTables.END_CITY_TREASURE_CHEST, 1, 0.05f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(LootTables.SNIFFER_DIGGING_GAMEPLAY, 1, 0.05f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
 
-        registerChestLoot(LootTables.END_CITY_TREASURE_CHEST, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(LootTables.BASTION_TREASURE_CHEST, 2, 0.2f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(LootTables.TRIAL_CHAMBERS_REWARD_CHEST, 2, 0.2f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
 
-        registerChestLoot(LootTables.STRONGHOLD_LIBRARY_CHEST, 1, 0.1f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(LootTables.ANCIENT_CITY_CHEST, 2, 0.3f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+
+        registerChestLoot(LootTables.TRIAL_CHAMBERS_REWARD_RARE_CHEST, 2, 0.4f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
     }
 }
