472dfedc7f3c608cd11aaa6b907d2da0d219e84f
Starman(StarmanMine142) <135715391+StarmanMine142@users.noreply.github.com>
2025-12-10 22:12:01 +0200

    Update ItemEntityMixin.java

--- src/main/java/net/davdeo/itemmagnetmod/mixin/ItemEntityMixin.java
+++ src/main/java/net/davdeo/itemmagnetmod/mixin/ItemEntityMixin.java
@@ -2,36 +2,43 @@ package net.davdeo.itemmagnetmod.mixin;
 
 import net.davdeo.itemmagnetmod.event.custom.PickupItemEvent;
 import net.davdeo.itemmagnetmod.util.ItemMagnetHelper;
+import net.davdeo.itemmagnetmod.config.ModConfig;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.TraceableEntity;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 @Mixin(value = ItemEntity.class, priority = 1002)
 public abstract class ItemEntityMixin extends Entity implements TraceableEntity {
-	@Unique
-	private static final double PICKUP_DISTANCE = 32.0;
-	@Unique
-	private static final double SQUARED_PICKUP_DISTANCE = PICKUP_DISTANCE * PICKUP_DISTANCE;
+
 	@Unique
 	private Player target;
 
 	protected ItemEntityMixin(EntityType<?> type, Level world) {
 		super(type, world);
 	}
+
+	@Unique
+	private double getPickupDistance() {
+		return ModConfig.getInstance().magnetDistance;
+	}
+
+	@Unique
+	private double getSquaredPickupDistance() {
+		double distance = getPickupDistance();
+		return distance * distance;
+	}
+
 
 	/*
 	 * Updates the target of the ItemEntity.
@@ -51,9 +58,9 @@ public abstract class ItemEntityMixin extends Entity implements TraceableEntity 
 		}
 
 		if (
-			this.target == null
-			|| this.target.distanceToSqr(thisObj) > SQUARED_PICKUP_DISTANCE
-			|| this.target != nextTarget
+				this.target == null
+						|| this.target.distanceToSqr(thisObj) > this.getSquaredPickupDistance() // ИСПОЛЬЗУЕТСЯ НАСТРОЙКА
+						|| this.target != nextTarget
 		) {
 			this.target = nextTarget;
 		}
@@ -65,22 +72,22 @@ public abstract class ItemEntityMixin extends Entity implements TraceableEntity 
 	 * The velocity towards the target is getting higher the closer the entity is to the target.
 	 * The logic used here is inspired by the behaviour of the ExperienceOrbEntity.
 	 *
-	 *  public void tick() {
+	 * public void tick() {
 	 * ...
-	 * 		this.prevX = this.getX();
-	 * 		this.prevY = this.getY();
-	 * 		this.prevZ = this.getZ();
-	 * 		Vec3d vec3d = this.getVelocity();
+	 * this.prevX = this.getX();
+	 * this.prevY = this.getY();
+	 * this.prevZ = this.getZ();
+	 * Vec3d vec3d = this.getVelocity();
 	 *
 	 * ---> Inject here. After call to getVelocity and before applying any movement
 	 *
-	 *      if (this.isTouchingWater() && this.getFluidHeight(FluidTags.WATER) > 0.10000000149011612) {
-	 *          this.applyWaterBuoyancy();
-	 *      } else if (this.isInLava() && this.getFluidHeight(FluidTags.LAVA) > 0.10000000149011612) {
-	 *          this.applyLavaBuoyancy();
-	 *      } else {
-	 *          this.applyGravity();
-	 *      }
+	 * if (this.isTouchingWater() && this.getFluidHeight(FluidTags.WATER) > 0.10000000149011612) {
+	 * this.applyWaterBuoyancy();
+	 * } else if (this.isInLava() && this.getFluidHeight(FluidTags.LAVA) > 0.10000000149011612) {
+	 * this.applyLavaBuoyancy();
+	 * } else {
+	 * this.applyGravity();
+	 * }
 	 * ...
 	 */
 	@Inject(method = "tick()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/item/ItemEntity;isInWater()Z"))
@@ -91,27 +98,31 @@ public abstract class ItemEntityMixin extends Entity implements TraceableEntity 
 			this.updateTarget();
 		}
 
+		double currentPickupDistance = this.getPickupDistance();
+		double currentSquaredPickupDistance = this.getSquaredPickupDistance();
+
 		// add to velocity depending on how far the items are away -> increased velocity, the closer the items get#
 		// Following logic was taken from ExperienceOrbEntity and slightly modified.
 		if (this.target != null) {
 			Vec3 targetEyeVector = new Vec3(this.target.getX() - thisObj.getX(), this.target.getY() + this.target.getEyeHeight() / 2.0 - thisObj.getY(), this.target.getZ() - thisObj.getZ());
 			double squaredTargetEyeDistance = targetEyeVector.lengthSqr();
 
-			if (squaredTargetEyeDistance < SQUARED_PICKUP_DISTANCE) {
-				double relativeTargetEyeDistance = 1.0 - Math.sqrt(squaredTargetEyeDistance) / PICKUP_DISTANCE;
+			if (squaredTargetEyeDistance < currentSquaredPickupDistance) {
+				double relativeTargetEyeDistance = 1.0 - Math.sqrt(squaredTargetEyeDistance) / currentPickupDistance;
 				thisObj.setDeltaMovement(thisObj.getDeltaMovement().add(targetEyeVector.normalize().scale(relativeTargetEyeDistance * relativeTargetEyeDistance * 0.1)));
 			}
 		}
 
 		if (
-			this.target != null &&
-			this.onGround() &&
-			this.getDeltaMovement().horizontalDistanceSqr() > 1.0E-5f &&
-			(this.tickCount + this.getId()) % 4 == 0
+				this.target != null &&
+						this.onGround() &&
+						this.getDeltaMovement().horizontalDistanceSqr() > 1.0E-5f &&
+						(this.tickCount + this.getId()) % 4 == 0
 		) {
 			thisObj.move(MoverType.SELF, thisObj.getDeltaMovement());
 		}
 	}
+
 
 	@Unique
 	private int stackCountBeforePickup = 0;
