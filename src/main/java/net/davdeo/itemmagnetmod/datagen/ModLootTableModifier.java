24edea4e66932fc1b3a2dc0a79bb058382f91a73
Starman(StarmanMine142) <135715391+StarmanMine142@users.noreply.github.com>
2025-12-10 13:04:08 +0200

    Update ModLootTableModifier.java

--- src/main/java/net/davdeo/itemmagnetmod/datagen/ModLootTableModifier.java
+++ src/main/java/net/davdeo/itemmagnetmod/datagen/ModLootTableModifier.java
@@ -2,46 +2,46 @@ package net.davdeo.itemmagnetmod.datagen;
 
 import net.davdeo.itemmagnetmod.item.ModItems;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
-import net.minecraft.item.Item;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.LootTables;
-import net.minecraft.loot.condition.RandomChanceLootCondition;
-import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.SetCountLootFunction;
-import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
-import net.minecraft.loot.provider.number.UniformLootNumberProvider;
-import net.minecraft.registry.RegistryKey;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
 
 public class ModLootTableModifier {
     private ModLootTableModifier() {
         super();
     }
 
-    private static void registerChestLoot(RegistryKey<LootTable> structure, int numberOfRolls, float chance, float minAmount, float maxAmount, Item item) {
+    private static void registerChestLoot(ResourceKey<LootTable> structure, int numberOfRolls, float chance, float minAmount, float maxAmount, Item item) {
         LootTableEvents.MODIFY.register(((id, tableBuilder, source, x) -> {
             if (source.isBuiltin() && structure.equals(id)) {
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(numberOfRolls))
-                        .conditionally(RandomChanceLootCondition.builder(chance))
-                        .with(ItemEntry.builder(item))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(minAmount, maxAmount)).build());
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .setRolls(ConstantValue.exactly(numberOfRolls))
+                        .when(LootItemRandomChanceCondition.randomChance(chance))
+                        .add(LootItem.lootTableItem(item))
+                        .apply(SetItemCountFunction.setCount(UniformGenerator.between(minAmount, maxAmount)).build());
 
-                tableBuilder.pool(poolBuilder);
+                tableBuilder.withPool(poolBuilder);
             }
         }));
     }
 
     public static void modifyLootTables() {
-        registerChestLoot(LootTables.BASTION_TREASURE_CHEST, 2, 0.35f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
-        registerChestLoot(LootTables.BASTION_BRIDGE_CHEST, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
-        registerChestLoot(LootTables.BASTION_HOGLIN_STABLE_CHEST, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
-        registerChestLoot(LootTables.BASTION_OTHER_CHEST, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(BuiltInLootTables.BASTION_TREASURE, 2, 0.35f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(BuiltInLootTables.BASTION_BRIDGE, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(BuiltInLootTables.BASTION_HOGLIN_STABLE, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(BuiltInLootTables.BASTION_OTHER, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
 
-        registerChestLoot(LootTables.ANCIENT_CITY_CHEST, 1, 0.35f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(BuiltInLootTables.ANCIENT_CITY, 1, 0.35f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
 
-        registerChestLoot(LootTables.END_CITY_TREASURE_CHEST, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(BuiltInLootTables.END_CITY_TREASURE, 1, 0.15f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
 
-        registerChestLoot(LootTables.STRONGHOLD_LIBRARY_CHEST, 1, 0.1f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
+        registerChestLoot(BuiltInLootTables.STRONGHOLD_LIBRARY, 1, 0.1f, 1.0f, 1.0f, ModItems.MAGNET_CORE);
     }
 }
