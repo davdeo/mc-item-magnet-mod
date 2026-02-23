4012f703e3fc1973bc64c9224da8c673f4f892ff
Starman <135715391+StarmanMine142@users.noreply.github.com>
2026-02-23 17:35:04 +0200

    Make Cloth Config and Mod Menu optional

--- src/main/java/net/davdeo/itemmagnetmod/config/ModConfig.java
+++ src/main/java/net/davdeo/itemmagnetmod/config/ModConfig.java
@@ -1,11 +1,6 @@
 package net.davdeo.itemmagnetmod.config;
 
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigCategory;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import me.shedaniel.clothconfig2.impl.builders.IntFieldBuilder;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,23 +9,17 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ModConfig {
-
     private static final String CONFIG_FILE_PATH = "config/itemmagnetmod.properties";
 
-    // Controls how far the magnet works
     public static int magnetDistance = 7;
-    // Controls the durability of the magnet
     public static int magnetDurability = 1024;
-    // Controls whether the magnet breaks when picking up items
     public static boolean isIndestructible = false;
-
-    // Controls whether a Magnet Core can be found in structures
     public static boolean canFindInBastion = true;
     public static boolean canFindInAncientCity = true;
     public static boolean canFindInEndCity = true;
     public static boolean canFindInStrongholdLibrary = true;
 
-    public static void loadConfig() {
+    public static void load() {
         Properties properties = new Properties();
         File configFile = new File(CONFIG_FILE_PATH);
 
@@ -38,26 +27,24 @@ public class ModConfig {
             try (FileInputStream input = new FileInputStream(configFile)) {
                 properties.load(input);
 
-                magnetDistance = Integer.parseInt(properties.getProperty("magnetDistance", String.valueOf(magnetDistance)));
-                magnetDurability = Integer.parseInt(properties.getProperty("magnetDurability", String.valueOf(magnetDurability)));
-                isIndestructible = Boolean.parseBoolean(properties.getProperty("isIndestructible", String.valueOf(isIndestructible)));
-                canFindInBastion = Boolean.parseBoolean(properties.getProperty("canFindInBastion", String.valueOf(canFindInBastion)));
-                canFindInAncientCity = Boolean.parseBoolean(properties.getProperty("canFindInAncientCity", String.valueOf(canFindInAncientCity)));
-                canFindInEndCity = Boolean.parseBoolean(properties.getProperty("canFindInEndCity", String.valueOf(canFindInEndCity)));
-                canFindInStrongholdLibrary = Boolean.parseBoolean(properties.getProperty("canFindInStrongholdLibrary", String.valueOf(canFindInStrongholdLibrary)));
+                magnetDistance = getInt(properties, "magnetDistance", magnetDistance);
+                magnetDurability = getInt(properties, "magnetDurability", magnetDurability);
+                isIndestructible = getBool(properties, "isIndestructible", isIndestructible);
+                canFindInBastion = getBool(properties, "canFindInBastion", canFindInBastion);
+                canFindInAncientCity = getBool(properties, "canFindInAncientCity", canFindInAncientCity);
+                canFindInEndCity = getBool(properties, "canFindInEndCity", canFindInEndCity);
+                canFindInStrongholdLibrary = getBool(properties, "canFindInStrongholdLibrary", canFindInStrongholdLibrary);
 
-            } catch (IOException | NumberFormatException e) {
-                System.err.println("Failed to load config file. Using defaults. " + e.getMessage());
-                saveConfig();
+            } catch (IOException e) {
+                System.err.println("[ItemMagnet] Failed to load config, using defaults.");
             }
         } else {
-            saveConfig();
+            save();
         }
     }
 
-    public static void saveConfig() {
+    public static void save() {
         Properties properties = new Properties();
-
         properties.setProperty("magnetDistance", String.valueOf(magnetDistance));
         properties.setProperty("magnetDurability", String.valueOf(magnetDurability));
         properties.setProperty("isIndestructible", String.valueOf(isIndestructible));
@@ -72,64 +59,16 @@ public class ModConfig {
         try (FileOutputStream output = new FileOutputStream(configFile)) {
             properties.store(output, "Item Magnet Mod Configuration");
         } catch (IOException e) {
-            System.err.println("Failed to save config file: " + e.getMessage());
+            System.err.println("[ItemMagnet] Failed to save config!");
         }
     }
 
-    public static Screen setupCloth(Screen parent) {
-        ConfigBuilder builder = ConfigBuilder.create()
-                .setTitle(Component.translatable("title.itemmagnetmod.config"))
-                .setParentScreen(parent)
-                .setSavingRunnable(ModConfig::saveConfig);
+    private static int getInt(Properties props, String key, int def) {
+        try { return Integer.parseInt(props.getProperty(key, String.valueOf(def))); }
+        catch (NumberFormatException e) { return def; }
+    }
 
-        ConfigCategory general = builder.getOrCreateCategory(Component.translatable("category.itemmagnetmod.general"));
-        ConfigCategory world = builder.getOrCreateCategory(Component.translatable("category.itemmagnetmod.world_gen"));
-        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-
-        IntFieldBuilder magnetDistanceField = entryBuilder.startIntField(Component.translatable("option.itemmagnetmod.magnet_distance"), magnetDistance)
-                .setDefaultValue(7)
-                .setTooltip(Component.translatable("tooltip.itemmagnetmod.magnet_distance"))
-                .setSaveConsumer(newValue -> magnetDistance = newValue)
-                .setMin(1)
-                .setMax(128);
-        general.addEntry(magnetDistanceField.build());
-
-        IntFieldBuilder magnetDurabilityField = entryBuilder.startIntField(Component.translatable("option.itemmagnetmod.magnet_durability"), magnetDurability)
-                .setDefaultValue(1024)
-                .setTooltip(Component.translatable("tooltip.itemmagnetmod.magnet_durability"))
-                .setSaveConsumer(newValue -> magnetDurability = newValue)
-                .setMin(1);
-        general.addEntry(magnetDurabilityField.build());
-
-        general.addEntry(entryBuilder.startBooleanToggle(Component.translatable("option.itemmagnetmod.indestructible"), isIndestructible)
-                .setDefaultValue(false)
-                .setTooltip(Component.translatable("tooltip.itemmagnetmod.indestructible"))
-                .setSaveConsumer(newValue -> isIndestructible = newValue)
-                .build());
-
-        world.addEntry(entryBuilder.startBooleanToggle(Component.translatable("option.itemmagnetmod.core_in_bastion"), canFindInBastion)
-                .setDefaultValue(true)
-                .setTooltip(Component.translatable("tooltip.itemmagnetmod.core_in_bastion"))
-                .setSaveConsumer(newValue -> canFindInBastion = newValue)
-                .build());
-
-        world.addEntry(entryBuilder.startBooleanToggle(Component.translatable("option.itemmagnetmod.core_in_ancient_city"), canFindInAncientCity)
-                .setDefaultValue(true)
-                .setTooltip(Component.translatable("tooltip.itemmagnetmod.core_in_ancient_city"))
-                .setSaveConsumer(newValue -> canFindInAncientCity = newValue)
-                .build());
-
-        world.addEntry(entryBuilder.startBooleanToggle(Component.translatable("option.itemmagnetmod.core_in_end_city"), canFindInEndCity)
-                .setDefaultValue(true)
-                .setTooltip(Component.translatable("tooltip.itemmagnetmod.core_in_end_city"))
-                .setSaveConsumer(newValue -> canFindInEndCity = newValue)
-                .build());
-
-        world.addEntry(entryBuilder.startBooleanToggle(Component.translatable("option.itemmagnetmod.core_in_stronghold_library"), canFindInStrongholdLibrary)
-                .setDefaultValue(true)
-                .setTooltip(Component.translatable("tooltip.itemmagnetmod.core_in_stronghold_library"))
-                .setSaveConsumer(newValue -> canFindInStrongholdLibrary = newValue)
-                .build());
-        return builder.build();
+    private static boolean getBool(Properties props, String key, boolean def) {
+        return Boolean.parseBoolean(props.getProperty(key, String.valueOf(def)));
     }
 }
