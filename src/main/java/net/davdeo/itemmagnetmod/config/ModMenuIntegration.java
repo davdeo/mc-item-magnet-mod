ae9a77738b4b9123a205769dca15984dab529bf1
Starman <135715391+StarmanMine142@users.noreply.github.com>
2026-02-23 17:36:35 +0200

    Make Cloth Config and Mod Menu optional

--- src/main/java/net/davdeo/itemmagnetmod/config/ModMenuIntegration.java
+++ src/main/java/net/davdeo/itemmagnetmod/config/ModMenuIntegration.java
@@ -2,10 +2,15 @@ package net.davdeo.itemmagnetmod.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.fabricmc.loader.api.FabricLoader;
 
 public class ModMenuIntegration implements ModMenuApi {
+
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ModConfig::setupCloth;
+        if (FabricLoader.getInstance().isModLoaded("cloth-config2")) {
+            return ModConfigGui::setupCloth;
+        }
+        return ModMenuApi.super.getModConfigScreenFactory();
     }
 }
